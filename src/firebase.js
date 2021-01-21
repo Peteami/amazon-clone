20070304962b84260627fe2d1e1be8bf2e5e859c
Peteami <peteami@hotmail.com>
2021-01-21 11:56:24 -0500

    Initial commit amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrSXKo2zp6oiCEtqwts_4xMmK6UO7f88Y",
+    authDomain: "clone-a957c.firebaseapp.com",
+    projectId: "clone-a957c",
+    storageBucket: "clone-a957c.appspot.com",
+    messagingSenderId: "418637615949",
+    appId: "1:418637615949:web:d86323e978b408f98d9d04"
+  };
