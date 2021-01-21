982723bd5990630dee55b58bb5364b7fe74cb13a
Peteami <peteami@hotmail.com>
2021-01-21 12:55:29 -0500

    Header content and styling but no functionnality yet

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import { ShoppingBasket } from '@material-ui/icons';
 
 function Header() {
     return (
@@ -7,11 +9,13 @@ function Header() {
             <img className="header__logo" 
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
             />
-            <div className="header__logo">
+
+            <div className="header__search">
                 <input 
                     className="header__searchInput"
                     type="text"
                 />
+                <SearchIcon className="header__searchIcon" />
             </div>
 
             <div className="header__nav">
@@ -19,7 +23,7 @@ function Header() {
                     <span className="header__optionLineOne">
                         Hello Guest
                     </span>
-                    <span className="header__optionLineOne">
+                    <span className="header__optionLineTwo">
                         Sign In
                     </span>
                 </div>
@@ -27,7 +31,7 @@ function Header() {
                 <span className="header__optionLineOne">
                         Returns
                     </span>
-                    <span className="header__optionLineOne">
+                    <span className="header__optionLineTwo">
                         Orders
                     </span>
                 </div>
@@ -35,10 +39,16 @@ function Header() {
                 <span className="header__optionLineOne">
                         Your
                     </span>
-                    <span className="header__optionLineOne">
+                    <span className="header__optionLineTwo">
                         Prime
                     </span>
                 </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasket />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
+
             </div>
         </div>
     )
